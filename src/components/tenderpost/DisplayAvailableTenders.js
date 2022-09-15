9d30d25d73edfc83e70b8db47d963d8f6aa71a8e
RonexLemon <ronexondimu@gmail.com>
2022-09-15 13:40:18 +0300

    modifier the tenders from javascript template literals

--- src/components/tenderpost/DisplayAvailableTenders.js
+++ src/components/tenderpost/DisplayAvailableTenders.js
@@ -8,34 +8,63 @@ import { useNavigate } from "react-router-dom";
 
 
 
-export const DisplayTenders =({tenders, deleteTender}) =>{
+ const DisplayTenders =(props) =>{
+    return <div className="card-container">
+
+{props.tenders.map((tender) =>(
+     <div className='tenderCard' >
+                  <div className='tenderCardHeader' id='tenderCardHeader'>
+                     
+                         
+                             <p><RiBuilding2Fill/><b> {tender.companyNames}</b></p>
+                             <p>{tender.tenderDescriptions}</p>
+                             <h4>{tender.tenderAmounts/1}</h4>
+                     
+                 </div>
+                 <div className='tenderCard-middle' id='tendercard-middle'>
+                     <h5><GiRotaryPhone/>&nbsp;{tender.contactEmails}&emsp;&emsp;&emsp;&emsp;<MdDateRange/>&nbsp;{tender.deadlineDates} &emsp;&emsp;&emsp;&emsp;<HiOutlineMail/>&nbsp;{tender.email}</h5>
+                  </div>
+                 <div className='bid-btn-approve-btn' id='bid-btn-approve-btn'>
+                     <button className='btn-bid' id='btn-bid' >BID</button>
+                     <button className='btn-aprove'>Approve</button>
+                     <button className="deletebtn" ><BsTrash/></button>
+                  </div>
+                 
+     
+              </div>
+               
+  ))}
+
+</div>
+};
+
    
-    const navigate = useNavigate();
+//     const navigate = useNavigate();
     
-    return tenders.map(tender => (
+//     return tenders.map(tender => (
         
-        <div className='tenderCard' key ={tender.contact}>
-            <div className='tenderCardHeader' id='tenderCardHeader'>
+//         <div className='tenderCard' key ={tender.contact}>
+//             <div className='tenderCardHeader' id='tenderCardHeader'>
                 
                     
-                        <p><RiBuilding2Fill/><b> {tender.companyName}</b></p>
-                        <p>{tender.description}</p>
-                        <h4>{tender.amount}</h4>
+//                         <p><RiBuilding2Fill/><b> {tender.companyName}</b></p>
+//                         <p>{tender.description}</p>
+//                         <h4>{tender.amount}</h4>
                 
-            </div>
-            <div className='tenderCard-middle' id='tendercard-middle'>
-                <h5><GiRotaryPhone/>&nbsp;{tender.contact}&emsp;&emsp;&emsp;&emsp;<MdDateRange/>&nbsp;{tender.deadline} &emsp;&emsp;&emsp;&emsp;<HiOutlineMail/>&nbsp;{tender.email}</h5>
-            </div>
-            <div className='bid-btn-approve-btn' id='bid-btn-approve-btn'>
-                <button className='btn-bid' id='btn-bid' onClick={ () =>navigate("/BiderForm")}>BID</button>
-                <button className='btn-aprove'>Approve</button>
-                <button className="deletebtn" onClick={()=> deleteTender(tender.contact)}><BsTrash/></button>
-            </div>
+//             </div>
+//             <div className='tenderCard-middle' id='tendercard-middle'>
+//                 <h5><GiRotaryPhone/>&nbsp;{tender.contact}&emsp;&emsp;&emsp;&emsp;<MdDateRange/>&nbsp;{tender.deadline} &emsp;&emsp;&emsp;&emsp;<HiOutlineMail/>&nbsp;{tender.email}</h5>
+//             </div>
+//             <div className='bid-btn-approve-btn' id='bid-btn-approve-btn'>
+//                 <button className='btn-bid' id='btn-bid' onClick={ () =>navigate("/BiderForm")}>BID</button>
+//                 <button className='btn-aprove'>Approve</button>
+//                 <button className="deletebtn" onClick={()=> deleteTender(tender.contact)}><BsTrash/></button>
+//             </div>
             
 
-        </div>
+//         </div>
           
-     ))
+//      ))
      
-  }
+//   }
   export default DisplayTenders;
