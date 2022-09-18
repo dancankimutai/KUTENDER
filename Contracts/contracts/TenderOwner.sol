f506aa75131cd4741fdda93c93dba20ded25734e
RonexLemon <ronexondimu@gmail.com>
2022-09-19 01:42:39 +0300

    implement approve tender

--- Contracts/contracts/TenderOwner.sol
+++ Contracts/contracts/TenderOwner.sol
@@ -3,8 +3,10 @@ pragma solidity  ^0.8.1;
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract TenderPoster{
+contract TenderPoster is Ownable{
     using  SafeMath for uint;
+    //enums for state
+    
 struct TenderDetails{
     address payable  owner;
     string companyName;
@@ -21,6 +23,7 @@ function writeTenderDetails(string memory _companyName,string memory _tenderDesc
     uint _tendersindex = tenderIndex;
     tenderItems[tenderIndex] = TenderDetails(payable(msg.sender),_companyName,_tenderDescription,_deadlineDate,_contactEmail,_tenderAmount,_tendersindex);
     tenderIndex = tenderIndex.add(1);
+    
 
 }
 
@@ -48,6 +51,13 @@ function readTenderDetails(uint _tenderIndex)public view returns (
 //function to return the number of tender items
 function tenderTotals()public view returns(uint){
     return tenderIndex;
+}
+//delete Tender
+function deleteTender(uint _index)public onlyOwner{
+    require(msg.sender != address(this),"Only The Owner can delete");
+    delete tenderItems[_index];
+
+
 }
 
 
